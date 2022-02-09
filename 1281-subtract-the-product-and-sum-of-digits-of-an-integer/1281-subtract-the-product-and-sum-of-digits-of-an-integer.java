ff791d2799bbfecb183e3b7ba012ed0ea1730ff7
Yashraj Jain <yashrajjain726@gmail.com>
2022-02-09 20:03:20 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (5.66%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -3,9 +3,7 @@ class Solution {
         int sumTemp = n;
         int productTemp = n;
         int sum = sumOfNumber(sumTemp);
-        System.out.println("sum "+sum);
         int product = productOfNumber(productTemp);
-        System.out.println("product "+product);
         int res = product - sum;
         return res;
     }
@@ -21,7 +19,6 @@ class Solution {
         
         if(n==0)
             return 1;
-        
         return n%10 * productOfNumber(n/10);
     }
 }
