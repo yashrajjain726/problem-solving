da3269414db9f977a189393e9f2aa048ecd2d76e
Yashraj Jain <yashrajjain726@gmail.com>
2022-02-11 21:08:51 +0530

    Time: 2 ms (44.32%), Space: 42.9 MB (16.50%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -5,17 +5,17 @@ class Solution {
         generateSubset(list,innerList,nums,0);
         return list;
     }
-    public void generateSubset(List<List<Integer>> list,List<Integer> innerList, int nums[],int i)
+    public void generateSubset(List<List<Integer>> list,List<Integer> innerList, int arr[],int ind)
     {
-       if(i==nums.length)
-       {
-           list.add(new ArrayList<Integer>(innerList));
-           return;
-       }
-        innerList.add(nums[i]);
-        generateSubset(list,innerList,nums,i+1);;
-        innerList.remove(innerList.size()-1);
-        generateSubset(list,innerList,nums,i+1);;
+       list.add(new ArrayList<>(innerList));
+        for(int i=ind;i<arr.length;i++)
+        {
+            if((i!=ind)&&(arr[i]==arr[i-1]))
+                continue;
+            innerList.add(arr[i]);
+            generateSubset(list,innerList,arr,i+1);
+            innerList.remove(innerList.size()-1);
+        }
         
     }
 }
