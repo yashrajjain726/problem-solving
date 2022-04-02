3dc103b3b960bbadf0a3aaf25c84d71003271716
Yashraj Jain <yashrajjain726@gmail.com>
2022-04-02 22:01:16 +0530

    Time: 1 ms (99.44%), Space: 41.9 MB (66.67%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray())
+        {
+            if(c=='(' || c=='{' || c=='[')
+                stack.push(c);
+            else if(
+                stack.isEmpty()==false
+                && ((c == '}' && stack.peek() == '{')||
+                    (c == ']' && stack.peek() == '[')||
+                    (c == ')' && stack.peek() == '('))
+                    )
+                stack.pop();   
+            else
+                return false;
+        }
+        return stack.isEmpty();
+        
+    }
+}
