da63311c26158ecf553fb507a2bc4f92ff9cf4cb
Yashraj Jain <yashrajjain726@gmail.com>
2022-08-22 13:28:50 +0530

    Time: 18 ms (23.11%), Space: 55.2 MB (7.26%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,20 +1,19 @@
 class Solution {
     public String decodeMessage(String key, String message) {
         Map<Character,Character> map = new HashMap<>();
-        char normal_char = 'a';
-        String res = "";
-        for(char ch:key.toCharArray())
-        {   if(ch==' ') continue;
-            if(map.containsKey(ch)==false)
-                map.put(ch,normal_char++);
+        char normal_char = 'a'; // for having a mapping structure as shown in example
+        String res = ""; // the result we want to return
+        for(char ch:key.toCharArray()) // looping through the key array
+        {   if(ch==' ') continue; // if a space occurs, leave it.
+            if(map.containsKey(ch)==false) // check it satisfies the condition of one occurence only
+                map.put(ch,normal_char++); // if yes, map it will the normal alapahbet structure
         }
-        System.out.println(map);
         for(char ch:message.toCharArray())
         {
             if(ch==' ')
-                res+=" ";
+                res+=" "; // add the spaces according to the message word
             else
-                res += map.get(ch);
+                res += map.get(ch); // add the alphabetic mapping that we odded
         }
         return res;
     }
